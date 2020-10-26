0624676ae2a28b3b7e68e75753dcaf2455681bfd
Jason Mwenyo <jasonmwenyo@gmail.com>
2020-10-26 11:52:56 +0200

    Playing around with git config

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const guessSubmit = document.querySelector('.guessSubmit');
 const guessField = document.querySelector('.guessField');
 
 let guessCount = 1;
-let resetButton;
+let resetButton ;
 
 function checkGuess() {
 
